397ddcfab288bee3c9ea6ee62a61355001d74c4e
msi1 <r.sekaleshfar@gmail.com>
2019-04-27 18:05:39 +0430

    add queries

--- app/src/main/java/com/lainexperiment/project02/db/CommentDao.java
+++ app/src/main/java/com/lainexperiment/project02/db/CommentDao.java
@@ -3,6 +3,7 @@ package com.lainexperiment.project02.db;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
 import android.arch.persistence.room.Update;
 
 import com.lainexperiment.project02.Comment;
@@ -21,4 +22,10 @@ public interface CommentDao
 
     @Delete
     void delete(Comment comment);
+
+    @Query("SELECT comment_table.id, comment_table.postId, comment_table.name, comment_table.email," +
+            " comment_table.body FROM comment_table INNER JOIN post_table" +
+            " ON comment_table.postId = post_table.id" +
+            " WHERE post_table.id = :postId")
+    void getAllPostComments(int postId);
 }
--- app/src/main/java/com/lainexperiment/project02/db/PostDao.java
+++ app/src/main/java/com/lainexperiment/project02/db/PostDao.java
@@ -3,9 +3,12 @@ package com.lainexperiment.project02.db;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
 import android.arch.persistence.room.Update;
 
 import com.lainexperiment.project02.Post;
+
+import java.util.List;
 
 @Dao
 public interface PostDao
@@ -21,4 +24,7 @@ public interface PostDao
 
     @Delete
     void delete(Post post);
+
+    @Query("SELECT * FROM post_table ORDER BY id ASC")
+    List<Post> getAllPosts();
 }
