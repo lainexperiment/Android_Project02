7dd8573b1fbf806752b6c36f0597737063bb1208
msi1 <r.sekaleshfar@gmail.com>
2019-04-27 17:13:16 +0430

    add database

---
+++ app/src/main/java/com/lainexperiment/project02/db/JTDatabase.java
@@ -0,0 +1,29 @@
+package com.lainexperiment.project02.db;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+import com.lainexperiment.project02.Comment;
+import com.lainexperiment.project02.Post;
+
+@Database(entities = {Post.class, Comment.class}, version = 1)
+public abstract class JTDatabase extends RoomDatabase
+{
+    private static JTDatabase instance;
+
+    public abstract CommentDao commentDao();
+    public abstract PostDao postDao();
+
+    public static synchronized JTDatabase getInstance(Context context)
+    {
+        if (instance == null)
+        {
+            instance = Room.databaseBuilder(context.getApplicationContext(), JTDatabase.class,
+                    "JT_database").fallbackToDestructiveMigration().build();
+        }
+
+        return instance;
+    }
+}
