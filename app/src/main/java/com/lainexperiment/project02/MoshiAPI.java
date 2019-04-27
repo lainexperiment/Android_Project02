c6f393fa6216c768453fac72fa4d4909f98cbb3c
Matin <m.zareimatin@gmail.com>
2019-04-27 23:15:42 +0430

    fix comment api url

--- app/src/main/java/com/lainexperiment/project02/MoshiAPI.java
+++ app/src/main/java/com/lainexperiment/project02/MoshiAPI.java
@@ -5,12 +5,13 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface MoshiAPI
 {
     @GET("posts")
     Call<List<Post>> getPosts();
 
-    @GET("posts/{postId}/comments")
-    Call<List<Comment>> getComments(@Path("postId") int postId);
+    @GET("comments")
+    Call<List<Comment>> getComments(@Query("postId") int postId);
 }
